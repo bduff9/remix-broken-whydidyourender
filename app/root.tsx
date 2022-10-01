d0e2e7389f0f44493fa672f6b0c2a10f6cb7a0af
bduff9 <bduff9@gmail.com>
2022-10-01 18:04:49 -0500

    Broken commit

---
+++ app/root.tsx
@@ -0,0 +1,41 @@
+import type { MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import whyDidYouRender from '@welldone-software/why-did-you-render';
+import React from "react";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+
+	if (typeof window !== 'undefined') {
+		// if (NODE_ENV !== 'production') {
+			whyDidYouRender(React);
+		// }
+	}
+
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/overall.tsx
@@ -0,0 +1,11 @@
+import { Link } from "@remix-run/react";
+import type { FC } from "react";
+
+const Overall: FC = () => (
+	<div>
+		<h1>Overall</h1>
+		<Link to="/">Home</Link>
+	</div>
+);
+
+export default Overall;
